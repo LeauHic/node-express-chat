074eb864fa6f2fb9245ce852f883f10cab699634
Loïck B <loickbrouard@yahoo.fr>
2022-12-21 15:12:33 +0100

    Chat basic fonctionnel

--- back-end/socket/chat.js
+++ back-end/socket/chat.js
@@ -2,7 +2,9 @@ module.exports = function (io) {
 
   io.on('connection', (socket) => {
     console.log(`Connecté au client ${socket.id}`)
-    io.emit('notification', { type: 'new_user', data: socket.id });
+    io.emit('new_user', { type: 'new_user', data: socket.id });
+    
+    io.emit("all", {sender:"DIEU", time:Date.now(), content:"Un nouvel utilisateur est apparu"});
 
     // Listener sur la déconnexion
     socket.on('disconnect', () => {
@@ -10,8 +12,11 @@ module.exports = function (io) {
       io.emit('notification', { type: 'removed_user', data: socket.id });
     });
 
-    socket.on('...', (msg) => {
-
+    socket.on('message_all', (msg) => {
+      console.log(msg);
+      msg.time = Date.now();
+      io.emit("all", msg);
     });
+
   })
 }
--- front-end/main.js
+++ front-end/main.js
@@ -1,14 +1,76 @@
+const defaultNickName = ["violetteduncan26", "Aonora", "OldGirlNoraArlani", "Ombreline", "Elias_of_Keliwich", "Fahliilyol", "ChiaraCadrich", "HawthorneAbendsen"];
+let me = defaultNickName[Math.floor(Math.random()*defaultNickName.length)];
+let socketId;
+const server = 'http://127.0.0.1:3000'
+const socket = io(server);
+
 (function () {
-    const server = 'http://127.0.0.1:3000'
-    const socket = io(server);
-
-    socket.on('notification', (data) => {
-        console.log('Message depuis le seveur:', data);
-    })
-
-    fetch(`${server}/test`).then((res) => {
-        return res.json()
-    }).then((data) => {
+    document.querySelector("#myName").value = me;
+    socket.on('all', (data) => {
         console.log(data);
+        addMessageToConversation(data.sender, data.time, data.content);
     })
+
+    socket.on('connect', (msg) => {
+        socketId = socket.id;
+    });
 })()
+
+let formulaire = document.forms.message_form;
+formulaire.addEventListener("submit", (e) => {
+    me = document.querySelector("#myName").value;
+    if(typeof me === 'undefined' || me == ""){
+        return;
+    }
+    e.preventDefault();
+    if (formulaire.message.value != "") {
+        emitMessage({
+            sender : me,
+            content : formulaire.message.value
+        });
+        formulaire.message.value = "";
+    }
+}, false);
+
+function emitMessage(message) {
+    socket.emit("message_all", message);
+}
+
+function addMessageToConversation(sender, time, content) {
+    let conversation = document.querySelector("#current_conv");
+
+    let name = document.createElement('div');
+    name.classList.add("name");
+
+    let name_span = document.createElement('span');
+    switch (sender) 
+    { 
+        case me:
+        name_span.classList.add("me"); 
+        name_span.innerText = "Moi";
+        break;
+
+        default:
+            name_span.innerText = sender;
+            break;
+    }
+
+    name.appendChild(name_span);
+
+    let message = document.createElement('div');
+    message.classList.add("message");
+
+    let message_content = document.createElement('p');
+    message_content.innerText = content;
+
+    let message_time = document.createElement('span');
+    message_time.classList.add("msg-time");
+    message_time.innerText = time
+    message.appendChild(message_content);
+    message.appendChild(message_time);
+    
+    let li = document.createElement('li');
+    li.appendChild(name);
+    li.appendChild(message);
+    conversation.appendChild(li);
+}
